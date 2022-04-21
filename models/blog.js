2f04f8ebfd6f380fdb73ee27e3420173ab1f77fb
Saymon Ahammad <101547798+Saymon-Ahammad@users.noreply.github.com>
2022-04-21 11:52:43 -0700

    Debugged some code

--- models/blog.js
+++ models/blog.js
@@ -1,9 +1,5 @@
-const express = require('express');
-const { links } = require('express/lib/response');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const blog = express();
-blog.use(express.static('public'));
 
 const blogSchema = new Schema({
   title: {
@@ -17,7 +13,7 @@ const blogSchema = new Schema({
   body: {
     type: String,
     required: true
-  }
+  },
 }, { timestamps: true });
 
 const Blog = mongoose.model('Blog', blogSchema);
