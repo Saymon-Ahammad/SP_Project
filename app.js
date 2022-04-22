16819e6b2d46fadfb2f9264de007da14f755adaf
Saymon Ahammad <101547798+Saymon-Ahammad@users.noreply.github.com>
2022-04-22 20:02:08 +0600

    added some features

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
 const result = require('lodash');
+const { render } = require('express/lib/response');
+
+//check
 
 // express app
 const app = express();
@@ -21,6 +24,7 @@ app.set('view engine', 'ejs');
 
 //middleware & static files
 app.use('/public', express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 
@@ -46,9 +50,35 @@ app.get('/blogs', (req, res) => {
    });
 });
 
+app.post('/blogs', (req, res) =>{
+  const blog = new Blog(req.body);
+
+  blog.save()
+   .then((result) => {
+     res.redirect('/blogs');
+   })
+   .catch((err) => {
+     console.log(err);
+   })
+
+})
+
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create a new Blog'});
 });
+
+app.get('/blogs/:id', (req,res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+   .then(result => {
+     res.render('details', { blog: result ,title: 'Blog Details'});
+   })
+   .catch(err => {
+     console.log(err);
+   });
+})
+
 
 // 404 page
 app.use((req, res) => {
